aa8a622506af7db4848633283bc3ec15a8b56553
Lukasz Nawojowski <luknwpl@gmail.com>
2017-03-18 11:58:46 +0100

    Initial commit
    React properties + Vector class

---
+++ src/Vector.js
@@ -0,0 +1,30 @@
+export default class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+equals(v) {
+  return this.x === v.x && this.y === v.y
+}
+
+add(v) {
+  return new Vector(this.x + v.x, this.y + v.y);
+}
+
+mod(size) {
+  return new Vector(this.x % size.x, this.y % size.y);
+}
+
+static rotateRight(vec){
+  return new Vector(vec.y, -vec.x);
+}
+
+static rotateLeft(vec){
+  return new Vector(-vec.y, vec.x);
+}
+
+static random(size) {
+  return new Vector(Math.floor(Math.random()  * size.x), Math.floor(Math.random * size.y));
+}
