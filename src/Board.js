0b24709c1c630f9ef93d1586548af647c3ef0b31
Lukasz Nawojowski <luknwpl@gmail.com>
2017-03-18 12:40:25 +0100

    Import packages

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import _ from "underscore";
+import classNames from "classnames";
+import Vector from "./Vector";
+import "./Board.css";
 
 export default class Board extends Component {
+  // React checks that the required property is declared in DOM
+  static propTypes = {
+    size: PropTypes.instanceOf(Vector)
+  };
+
   render() {
     return ( <div>x: {this.props.size.x}, y:{this.props.size.y}</div> );
   }
