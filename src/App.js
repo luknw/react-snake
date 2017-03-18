abe61fb52d871d6ff3f1d0cedc0fb0d7aa55f240
Lukasz Nawojowski <luknwpl@gmail.com>
2017-03-18 14:02:45 +0100

    React to events

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import Board from './Board';
 
 
 export default class App extends Component {
+  state = {
+    snakePositions: []
+  };
+
   // component rendered, we add reactive stuff
   componentDidMount() {
     const ticks = Bacon.interval(100);
     // log from Bacon with <3
-    const directions = ticks.map(() => new Vector(0, 1));
-    directions.log();
+    // ticks.log();
+
+    const keys = Bacon.fromEvent(document.body, "keyup");
+    const lefts = keys.filter(key => key.keyCode === 37);
+    const rights = keys.filter(key => key.keyCode === 39);
+    const leftRotations = lefts.map(() => Vector.rotateLeft);
+    const rightRotations = rights.map(() => Vector.rotateRight);
+    const actions = leftRotations.merge(rightRotations);
+    actions.log();
+    const directions = actions.scan(new Vector(0, 1), (dir, f) => f(dir));
+
+    const snakeHeadPositions = directions
+      .sampledBy(ticks)
+      .scan(new Vector(0, 0), (pos, dir) =>
+        pos.add(dir).mod(new Vector(20, 20)));
+    snakeHeadPositions.onValue(head =>
+       this.setState({ snakePositions: [head] }))
   }
 
   render() {
     return ( <Board
        size={new Vector(20, 20)}
-       snakePositions={[new Vector(1,2)]}
+       snakePositions={this.state.snakePositions}
        fruitPosition={new Vector(5, 5)} /> );
   }
 }
